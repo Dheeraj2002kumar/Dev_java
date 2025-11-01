009649298ac49d55004d17d01bea05cfc9b498de
Dheeraj Kumar <dk8549644@gmail.com>
2025-11-01 19:45:36 +0530

    Initial commit

---
+++ src/Variable.java
@@ -0,0 +1,13 @@
+public class Variable {
+    public static void main(String[] args){
+        System.out.println("Hello Developer...");
+
+        // Declaration
+        int a;
+        String name;
+
+
+
+
+    }
+}

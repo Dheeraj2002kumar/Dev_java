029f16aa25d969839562d0ab818a602dad4b9b9e
Dheeraj Kumar <dk8549644@gmail.com>
2025-11-01 20:08:28 +0530

    updated variable

--- src/Variable.java
+++ src/Variable.java
@@ -1,3 +1,7 @@
+/**
+ * This is JavaDoc Comment
+ * */
+
 public class Variable {
     public static void main(String[] args){
         System.out.println("Hello Developer...");
@@ -15,6 +19,19 @@ public class Variable {
         System.out.println("Hello " + name + " , Your age " + age + "...");
         System.out.println("You are a developer...");
 
+        // your name, your age, four favourite programming language
+        name = "Dheeraj";
+        age = 23;
+        String language = "Java";
+        System.out.println("Hello " + name + ". Your age " + age + " Your favourite language " + language);
 
+        // final --> is used to make constant
+        final int CONSTANT_VARIABLE;
+
+        /*
+        Multi
+        line
+        comment
+         */
     }
 }
